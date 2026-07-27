941eb01f340e0ba72280a87a7b5b0978a8f4cab9
Aalxey <infurnus555@gmail.com>
2026-07-27 17:08:33 +0545

    soul awakened

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from config import DISCORD_TOKEN
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has awakened!")
+
+
+bot.run(DISCORD_TOKEN)
